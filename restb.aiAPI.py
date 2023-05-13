2f7fd39451518075499570f11f2598cd9ab8d5fc
Andreu Pérez <apt15udl@gmail.com>
2023-05-13 18:19:53 +0200

    noPrints

--- restb.aiAPI.py
+++ restb.aiAPI.py
@@ -32,7 +32,6 @@ def jsonFormat():
             }
         }
     }
-    #print(data)
 
     with open("datos.json", "w") as outfile:
         json.dump(data, outfile)
@@ -46,7 +45,6 @@ def api():
     'client_key': '2593c868c3e81326f28f6eab1c97191f70017da8908b0dec17d8e60cfc690e49'
     }
     urls = sys.argv[5].split(",")
-    #print(urls)
     request_body = {
     "image_urls": urls,
     "solutions": {"roomtype": 1.0, "roomtype_reso": 1.0, "style": 1.0, "r1r6": None, "c1c6": None, "features": 4.0, "features_reso": 1.0, "compliance": 2.0, "caption": None}
@@ -57,7 +55,6 @@ def api():
    
     # The response is formatted in JSON
     json_response = response.json()
-    print(json_response)
 
 
 def readJson():
