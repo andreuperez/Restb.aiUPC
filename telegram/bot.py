e4cf06e16d899316164df78d7aa5f96f4a048220
peremunoz <peremunozfiguerol33@gmail.com>
2023-05-14 05:13:44 +0200

    Solved

--- telegram/bot.py
+++ telegram/bot.py
@@ -7,21 +7,21 @@ import subprocess
 
 
 
-HOSTNAME = ""
+HOSTNAME = "https://600c-147-83-201-134.ngrok-free.app"
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 #responde al conmando /strat
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
-    global id
     text_html = '<b>Benvingut al Bot de Telegram!</b>' + '\n' + 'Escriviu la comanda /consulta per a realitzar una nova consulta.'  + '\n' + 'Escriviu la comanda /processar per a processar la consulta.'
-    id = message.chat.id
     bot.send_message(message.chat.id, text_html, parse_mode="html", disable_web_page_preview=True)
 
 @bot.message_handler(commands=["consulta"])
 def cmd_start(message):
+    global id
     markup = ForceReply()
     msg = bot.reply_to(message, "Introdueixi el codi postal", reply_markup=markup)
+    id = message.chat.id
     bot.register_next_step_handler(msg, preguntar_m2)
 
 def preguntar_m2(message):
@@ -48,6 +48,7 @@ def bot_mensajes_texto(message):
     url_name = HOSTNAME + "/images/" +  file_info.file_path.split("/")[1]
     names.append(url_name)
     print(names)
+
     with open(name, "wb") as f:
         f.write(file_bytes)
 
@@ -56,14 +57,16 @@ def cmd_start(message):
     global names, codi, m2, id
     # Ejecutar mi_script.py
     
-    p = subprocess.Popen(["python", "restb.aiAPI.py", codi, m2] + names, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(["python3", "./restb.aiAPI.py", codi, m2] + names, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Obtener la salida del script
     out, err = p.communicate()
 
+    p.wait()
+
     # Imprimir la salida
     price = out.decode("utf-8")
-    text_html = '<b>' + price + '</b>' 
+    text_html = '<b>' + price + '</b>'
     bot.send_message(id, text_html, parse_mode="html", disable_web_page_preview=True)
     names = []
 
