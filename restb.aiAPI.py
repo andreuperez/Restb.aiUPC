80f574cfc9e8e56cdd4fb4767112733016b2a27c
Andreu Pérez <apt15udl@gmail.com>
2023-05-14 02:37:06 +0200

    Finalv3

--- restb.aiAPI.py
+++ restb.aiAPI.py
@@ -42,7 +42,7 @@ def api():
     # Add your client key
     'client_key': '2593c868c3e81326f28f6eab1c97191f70017da8908b0dec17d8e60cfc690e49'
     }
-    urls = sys.argv[5].split(",")
+    urls = sys.argv[3].split(",")
     request_body = {
     "image_urls": urls,
     "solutions": {"roomtype": 1.0, "roomtype_reso": 1.0, "style": 1.0, "r1r6": None, "c1c6": None, "features": 4.0, "features_reso": 1.0, "compliance": 2.0, "caption": None}
