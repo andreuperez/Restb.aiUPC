d9092d42a205506d5d27c888beab8d0a92d33a1b
Andreu Pérez <apt15udl@gmail.com>
2023-05-14 04:33:59 +0200

    v1

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,51 +1,71 @@
 from config import *
 import telebot
 from PIL import Image
+from telebot.types import ReplyKeyboardMarkup
+from telebot.types import ForceReply
+import subprocess
 
+
+
+HOSTNAME = ""
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 #responde al conmando /strat
-@bot.message_handler(commands=["start", "ayuda", "help"])
+@bot.message_handler(commands=["start"])
 def cmd_start(message):
-    """Da la bienvenida al usuario del bot"""
-    bot.reply_to(message, "Hola!")
+    text_html = '<b>Benvingut al Bot de Telegram!</b>' + '\n' + 'Escriviu la comanda /consulta per a realitzar una nova consulta.'  + '\n' + 'Escriviu la comanda /processar per a processar la consulta.'
+    bot.send_message(message.chat.id, text_html, parse_mode="html", disable_web_page_preview=True)
 
-#Responder a los mensajes de textos que no son comandos
-@bot.message_handler(content_types=["text"])
-def bot_mensajes_texto(message):
-    if message.text.startswith("/"): #Per a que no agafi comandes
-        bot.send_message(message.chat.id, "Comanda no disponible!")
-    else:
-        bot.send_message(message.chat.id, "Programem!")
+@bot.message_handler(commands=["consulta"])
+def cmd_start(message):
+    markup = ForceReply()
+    msg = bot.reply_to(message, "Introdueixi el codi postal", reply_markup=markup)
+    bot.register_next_step_handler(msg, preguntar_m2)
+
+def preguntar_m2(message):
+    global codi
+    codi = message.text
+    markup = ForceReply()
+    msg = bot.reply_to(message, "Introdueixi els m2 de l'habitatge", reply_markup=markup)
+    bot.register_next_step_handler(msg, preguntar_imatges)
+
+def preguntar_imatges(message):
+    global m2
+    m2 = message.text
+    markup = ForceReply()
+    msg = bot.reply_to(message, "Introdueixi les imatges de l'habitatge", reply_markup=markup)
 
 @bot.message_handler(content_types=["photo"])
 def bot_mensajes_texto(message):
-
+    global names
     photo_size = message.photo[-1]
-    # Descargamos el archivo en un arreglo de bytes
-    
     file_info = bot.get_file(photo_size.file_id)
     print(file_info.file_path)
     file_bytes = bot.download_file(file_info.file_path)
-    with open("archivo_descargado.jpg", "wb") as f:
+    name = "./public/images/" + file_info.file_path.split("/")[1]
+    url_name = HOSTNAME + "/images/" +  file_info.file_path.split("/")[1]
+    names.append(url_name)
+    print(names)
+    with open(name, "wb") as f:
         f.write(file_bytes)
 
-    # Descargamos la imagen en miniatura
+@bot.message_handler(commands=["processar"])
+def cmd_start(message):
+    global names, codi, m2
+    # Ejecutar mi_script.py
     
+    p = subprocess.Popen(["python", "restb.aiAPI.py", codi, m2] + names, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    '''
-    # Obtenemos el objeto File correspondiente a la imagen en miniatura
-    photo_file = bot.get_file(photo_size.file_id)
+    # Obtener la salida del script
+    out, err = p.communicate()
 
-    # Descargamos el archivo en un arreglo de bytes
-    photo_data = photo_file.download_as_bytearray()
-
-    # Guardamos los datos en un archivo
-    with open('miniatura.jpg', 'wb') as f:
-        f.write(photo_data)
-    '''
+    # Imprimir la salida
+    print(out.decode("utf-8"))
     
 
 if __name__ == '__main__':
-    print('Inicialndo el bot')
+    global names
+    print('Iniciando el bot')
+    names = list()
     bot.infinity_polling()
+    
